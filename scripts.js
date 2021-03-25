ccc6ee3fb841e99fea712a8b567389b7df3ee3ca
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2021-03-25 13:40:20 -0700

    Made dice display more obvious as to what it was.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 function rollDice(){
-    document.getElementById("dice-display").innerHTML = Math.floor(Math.random() * 6) + 1
+    document.getElementById("dice-display").innerHTML = `You rolled: ${Math.floor(Math.random() * 6) + 1}`
 }
